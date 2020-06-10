070767200e265a49cd31f9a0c19673bf5cf79d13
hiro <hi.kanno23@gmail.com>
2020-06-10 11:53:33 +0900

    重複解消

--- html/wp-content/themes/myfirsttheme/index.php
+++ html/wp-content/themes/myfirsttheme/index.php
@@ -48,11 +48,5 @@ $qa_posts = get_posts($args);
     <div id="calendar"></div>
   </div>
 </section>
-<section>
-  <div class="container">
-    <h2><?php myfirstplugin_say_hello(); ?></h2>
-    <div id="calendar"></div>
-  </div>
-</section>
 
 <?php get_footer(); ?>
